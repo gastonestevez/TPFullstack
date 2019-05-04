1d0f986df745dd7a3c2c63038fa18b9c8828c2f8
Gaston <gaston.estevez7@gmail.com>
2019-05-04 16:11:47 -0400

    cambio de nombre login.html a login.php

---
+++ Proyecto integrador/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+      <meta charset="utf-8">
+    <link rel="stylesheet" href="css/normalize.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC" rel="stylesheet">
+		<link rel="stylesheet" href="css/styles.css">
+    <title>BIGFASHION</title>
+  </head>
+     <div class="login">
+       <img src="img/img_2929.jpg" alt="logo">
+        <h2>Mi cuenta en <strong>BIGFASHION</strong></h2>
+        <h3> ¿Ya eres cliente?</h3>
+        <h4> ¡Qué bueno verte!</h4><br>
+          <div class="datos">
+            <form class="login" action="index.html" method="post">
+              <input type="email" name="email" value="" placeholder="Ingresa tu correo electrónico"><br><br>
+              <input type="password" name="contraseña" value="" placeholder="Ingresa tu contraseña"><br><br>
+            <section class="recordame">
+              <input type="checkbox" name="recordame" value="">
+              <label for="recordame">recordame</label>
+            </section>
+              <button type="submit" name="enviar">ingresar</button>
+          </div>
+            </form>
+  </body>
+</html>
