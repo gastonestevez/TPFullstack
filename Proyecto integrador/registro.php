10819fedf41be6274931400836e64a41e0420ece
naylasofia <50183590+naylasofia@users.noreply.github.com>
2019-05-06 00:46:13 -0300

    Add files via upload

---
+++ Proyecto integrador/registro.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <header>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC" rel="stylesheet">
+		<link rel="stylesheet" href="css/styles.css">
+    <title>BIGFASHION</title>
+  </header>
+  <section class="general">
+       <div class="registro">
+       <img src="img/img_2929.jpg" alt="logo">
+       <div class="titulos">
+        <h2>Mi cuenta en <strong>BIGFASHION</strong></h2>
+        <h3> Registrate</h3>
+        <h4> y enterate de nuestras novedades y Ofertas!</h4>
+        </div>
+        <div class="campos">
+            <form class="registracion" action="index.html" method="post">
+              <h5>Nombre</h5>
+              <input id="nombre" type="text" name="nombre" value="" placeholder="Ingresa tu nombre"><br><br>
+              <h5>Apellido</h5>
+              <input id="apellido" type="text" name="apellido" value="" placeholder="Ingresa tu Apellido"><br><br>
+              <h5>E-mail</h5>
+              <input id="mail" type="mail" name="mail" value="" placeholder="Ingresa tu e-mail"><br><br>
+              <h5>Fecha de nacimiento</h5>
+              <input id="fechanacimiento" type="date" name="fechanacimiento" value="" ><br><br>
+              <h5>Provincia</h5>
+              <label for="Provincia"></label>
+              <select class="" name="provincia" id="provincia">
+                <option value="" >Selecciona tu provincia</option>
+                <option value="Buenos Aires">Bs. As.</option>
+                <option value="Catamarca">Catamarca</option>
+                <option value="Chaco">Chaco</option>
+                <option value="Chubut">Chubut</option>
+                <option value="Cordoba">Cordoba</option>
+                <option value="Corrientes">Corrientes</option>
+                <option value="Entre Rios">Entre Rios</option>
+                <option value="Formosa">Formosa</option>
+                <option value="Jujuy">Jujuy</option>
+                <option value="La Pampa">La Pampa</option>
+                <option value="La Rioja">La Rioja</option>
+                <option value="Mendoza">Mendoza</option>
+                <option value="Misiones">Misiones</option>
+                <option value="Neuquen">Neuquen</option>
+                <option value="Rio Negro">Rio Negro</option>
+                <option value="Salta">Salta</option>
+                <option value="San Juan">San Juan</option>
+                <option value="San Luis">San Luis</option>
+                <option value="Santa Cruz">Santa Cruz</option>
+                <option value="Santa Fe">Santa Fe</option>
+                <option value="Sgo. del Estero">Sgo. del Estero</option>
+                <option value="Tierra del Fuego">Tierra del Fuego</option>
+                <option value="Tucuman">Tucuman</option>
+                    </select>
+                <h5>Ingresa tu password</h5>
+                <input id="pass" type="password" name="pass" value><br><br>
+                <h5>Ingresa tu password nuevamente</h5>
+                <input id="pass" type="password" name="pass" value><br><br>
+              <div class="novedades">
+              <input type="checkbox" name="novedades" value="">
+              <label for="novedades">Te gustaria recibir novedades de BIG FASHION por mail? </label>
+              <button type="submit" name="resgistro">Registrarme</button>
+              </div>
+            </section>
+
+          </div>
+          </div>
+            </form>
+            </section>
+  </body>
+</html>
