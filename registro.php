013999285ed3f5278e7379e372027d2d85bb099d
gaston <gaston.estevez7@gmail.com>
2019-05-08 15:22:07 -0300

    pruebo aplicar bootstrap a registro.php y cambio fondo

--- registro.php
+++ registro.php
@@ -8,55 +8,70 @@
         <h4>y enterate de nuestras novedades y Ofertas!</h4>
       </div>
 
-      <div class="campos">
-        <form class="registracion" action="index.html" method="post">
-              <h5>Nombre</h5>
-              <input id="nombre" type="text" name="nombre" value="" placeholder="Ingresa tu nombre"><br><br>
-              <h5>Apellido</h5>
-              <input id="apellido" type="text" name="apellido" value="" placeholder="Ingresa tu Apellido"><br><br>
-              <h5>E-mail</h5>
-              <input id="mail" type="mail" name="mail" value="" placeholder="Ingresa tu e-mail"><br><br>
-              <h5>Fecha de nacimiento</h5>
-              <input id="fechanacimiento" type="date" name="fechanacimiento" value="" ><br><br>
-              <h5>Provincia</h5>
-              <label for="Provincia"></label>
-              <select class="" name="provincia" id="provincia">
-                <option value="" >Selecciona tu provincia</option>
-                <option value="Buenos Aires">Bs. As.</option>
-                <option value="Catamarca">Catamarca</option>
-                <option value="Chaco">Chaco</option>
-                <option value="Chubut">Chubut</option>
-                <option value="Cordoba">Cordoba</option>
-                <option value="Corrientes">Corrientes</option>
-                <option value="Entre Rios">Entre Rios</option>
-                <option value="Formosa">Formosa</option>
-                <option value="Jujuy">Jujuy</option>
-                <option value="La Pampa">La Pampa</option>
-                <option value="La Rioja">La Rioja</option>
-                <option value="Mendoza">Mendoza</option>
-                <option value="Misiones">Misiones</option>
-                <option value="Neuquen">Neuquen</option>
-                <option value="Rio Negro">Rio Negro</option>
-                <option value="Salta">Salta</option>
-                <option value="San Juan">San Juan</option>
-                <option value="San Luis">San Luis</option>
-                <option value="Santa Cruz">Santa Cruz</option>
-                <option value="Santa Fe">Santa Fe</option>
-                <option value="Sgo. del Estero">Sgo. del Estero</option>
-                <option value="Tierra del Fuego">Tierra del Fuego</option>
-                <option value="Tucuman">Tucuman</option>
-              </select>
-                <h5>Ingresa tu password</h5>
-                <input id="pass" type="password" name="pass" value><br><br>
-                <h5>Ingresa tu password nuevamente</h5>
-                <input id="pass" type="password" name="pass" value><br><br>
-              <div class="novedades">
-                <input type="checkbox" name="novedades" value="">
-                <label for="novedades">Te gustaria recibir novedades de BIG FASHION por mail? </label>
+        <form class="formRegistro"  action="success.php" method="post">
+          <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputNombre">Nombre</label>
+                <input id="nombre" type="text" class="form-control" name="nombre" placeholder="Ingresa tu nombre">
               </div>
-                <button class="btn btn-dark d-block mx-auto" type="submit" name="resgistro">Registrarme</button>
+              <div class="form-group col-md-6">
+                <label for="inputApellido">Apellido</label>
+                <input id="apellido" type="text" class="form-control" name="apellido" placeholder="Ingresa tu Apellido">
+              </div>
+          </div>
+              <div class="form-group">
+                <label for="inputEmail">E-mail</label>
+                <input id="mail" type="email" name="mail" class="form-control" placeholder="Ingresa tu e-mail">
+              </div>
+              <div class="form-group">
+                <label for="inputCumple">Fecha de nacimiento</label>
+                <input id="fechanacimiento" type="date" name="fechanacimiento" class="form-control" >
+              </div>
+              <div class="form-group">
+              <label for="inputProvincia">Provincia</label>
+                <select class="form-control" name="provincia" id="provincia">
+                  <option value="" >Selecciona tu provincia</option>
+                  <option value="Buenos Aires">Bs. As.</option>
+                  <option value="Catamarca">Catamarca</option>
+                  <option value="Chaco">Chaco</option>
+                  <option value="Chubut">Chubut</option>
+                  <option value="Cordoba">Cordoba</option>
+                  <option value="Corrientes">Corrientes</option>
+                  <option value="Entre Rios">Entre Rios</option>
+                  <option value="Formosa">Formosa</option>
+                  <option value="Jujuy">Jujuy</option>
+                  <option value="La Pampa">La Pampa</option>
+                  <option value="La Rioja">La Rioja</option>
+                  <option value="Mendoza">Mendoza</option>
+                  <option value="Misiones">Misiones</option>
+                  <option value="Neuquen">Neuquen</option>
+                  <option value="Rio Negro">Rio Negro</option>
+                  <option value="Salta">Salta</option>
+                  <option value="San Juan">San Juan</option>
+                  <option value="San Luis">San Luis</option>
+                  <option value="Santa Cruz">Santa Cruz</option>
+                  <option value="Santa Fe">Santa Fe</option>
+                  <option value="Sgo. del Estero">Sgo. del Estero</option>
+                  <option value="Tierra del Fuego">Tierra del Fuego</option>
+                  <option value="Tucuman">Tucuman</option>
+                </select>
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="inputPassword">Contraseña</label>
+                  <input id="pass" class="form-control" type="password" name="pass" value>
+                </div>
+                <div class="form-group col-md-6">
+                  <label>Ingresa tu contraseña nuevamente</label>
+                  <input id="passconf" class="form-control" type="password" name="passconf" value>
+                </div>
+              </div>
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="novedades" value="">
+                <label for="novedades" class="form-check-label">Te gustaria recibir novedades de <strong>BIG FASHION</strong> por mail? </label>
+              </div>
+                <button class="btn btn-dark d-block mx-auto mt-4 " type="submit" name="resgistro">Registrarme</button>
         </form>
-      </div>
     </div>
   </div>
 </section>
