3626285ce1f964f6f3070ba3d90725af14bde35d
andreabag <50058026+andreabag@users.noreply.github.com>
2019-06-04 14:44:12 -0300

    Add files via upload
    modifico home

--- homepage.php
+++ homepage.php
@@ -1,37 +1,16 @@
 <main class="py-4 tarjetaPrincipal">
 <div class="container-fluid">
-      <div class="card border border-dark rounded col-sm-12 col-md-6" style="width: 18rem;">
+      <div class="card border border-dark rounded col-sm-12 col-md-6 mx-auto" style="width: 18rem;">
           <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="card-img-top d-block w-100" src="img/IMG_2929.jpg" alt="Portada Logo">
+                    <img class="card-img-top d-block w-100" src="img/img_2929.jpg" alt="Portada Logo">
                   </div>
                 <div class="carousel-item">
-                  <img class="card-img-top d-block w-100" src="img/card_sample1.png" alt="Portada slide">
+                  <img class="card-img-top d-block w-100" src="img/20desc.png" alt="Portada slide">
                 </div>
                 <div class="carousel-item">
-                  <img class="card-img-top d-block w-100" src="img/card_sample2.png" alt="Portada 2 slide">
-                </div>
-              </div>
-            </div>
-          <div class="card-body ">
-            <h4 class="card-title text-center">Grandes hombres</h5>
-            <h6 class="card-subtitle mb-2 text-muted text-center">Tu mejor versión</h6>
-            <p class="card-text text-center">En Big Fashion encontrarás la mejor ropa <strong>casual</strong> e <strong>informal</strong> para que te sientas super cómodo en todo momento.</p>
-            <a href="login.php" class="btn btn-primary d-block mx-auto">¡Empecemos!</a>
-          </div>
-      </div>
-      <div class="card  border border-dark rounded col-sm-12 col-md-6" style="width: 18rem;">
-          <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="card-img-top d-block w-100" src="img/IMG_2929.jpg" alt="Portada Logo">
-                  </div>
-                <div class="carousel-item">
-                  <img class="card-img-top d-block w-100" src="img/card_sample1.png" alt="Portada slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="card-img-top d-block w-100" src="img/card_sample2.png" alt="Portada 2 slide">
+                  <img class="card-img-top d-block w-100" src="img/invierno.png" alt="Portada 2 slide">
                 </div>
               </div>
             </div>
@@ -43,5 +22,11 @@
           </div>
       </div>
       </div>
-
+      <footer class="main-footer">
+  			<ul>
+  				<li><img class="envios" src="img/envios1.png" alt="envios_gratis"></li>
+  				<li><img class="envios" src="img/envioscasa.png" alt="envios_domicilio"></li>
+  				<li><img class="envios" src="img/mercadopago.png" alt="mercadopago"></li>
+  			</ul>
+  		</footer>
   </main>
