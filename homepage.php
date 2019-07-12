f3fdcd07e9584ef70905da9eae40acd7678c53b4
naylasofia <50183590+naylasofia@users.noreply.github.com>
2019-07-12 20:12:07 -0300

    actualizacion de Homepage + css

--- homepage.php
+++ homepage.php
@@ -1,36 +1,24 @@
 <main class="py-4 tarjetaPrincipal">
 <div class="container-fluid">
-  
-  <div class="card-deck">
 
-    <div class="card border border-dark rounded" style="width: 18rem;">
-        <img class="card-img-top d-block w-100" src="img/20desc.png" alt="Portada Logo">
-        <div class="card-body ">
-          <h4 class="card-title text-center">Grandes Promociones</h5>
-          <h6 class="card-subtitle mb-2 text-muted text-center">Lorem ipsum dolor sit amet.</h6>
-          <p class="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur labore quae vero ipsum, facilis quam sequi accusamus accusantium?</p>
-          <a href="login.php" class="btn btn-primary d-block mx-auto">Ver Productos</a>
-        </div>
+
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-90" src="img/miko2019.jpg" alt="First slide">
     </div>
-
-    <div class="card border border-dark rounded" style="width: 18rem;">
-        <img class="card-img-top d-block w-100" src="img/img_2929.jpg" alt="Portada slide">
-        <div class="card-body ">
-          <h4 class="card-title text-center">Grandes Hombres</h5>
-          <h6 class="card-subtitle mb-2 text-muted text-center">Tu mejor versión</h6>
-          <p class="card-text text-center">En Big Fashion encontrarás la mejor ropa <strong>casual</strong> e <strong>informal</strong> para que te sientas super cómodo en todo momento. Lorem ipsum dolor sit amet.</p>
-          <a href="login.php" class="btn btn-primary d-block mx-auto">¡Empecemos!</a>
-        </div>
+    <div class="carousel-item">
+      <img class="d-block w-90" src="img/2.jpg" alt="Second slide">
     </div>
-
-    <div class="card border border-dark rounded" style="width: 18rem;">
-        <img class="card-img-top d-block w-100" src="img/card_sample2.png" alt="Portada slide">
-        <div class="card-body ">
-          <h4 class="card-title text-center">Grandes Compras</h5>
-          <h6 class="card-subtitle mb-2 text-muted text-center">Lorem ipsum dolor sit amet.</h6>
-          <p class="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed exercitationem facilis omnis nesciunt eveniet iste suscipit incidunt harum. Ratione.</p>
-          <a href="login.php" class="btn btn-primary d-block mx-auto">Servicios</a>
-        </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     </div>
 
   </div>
@@ -39,8 +27,8 @@
 
       <footer class="main-footer">
         <ul>
-          <li><img class="envios" src="img/envios1.png" alt="envios_gratis"></li>
-  				<li><img class="envios" src="img/envioscasa.png" alt="envios_domicilio"></li>
+          <li><img class="envios" src="img/enviosgratis.png" alt="envios_gratis"></li>
+  				<li><img class="envios" src="img/envio.png" alt="envios_domicilio"></li>
   				<li><img class="envios" src="img/mercadopago.png" alt="mercadopago"></li>
   			</ul>
   		</footer>
