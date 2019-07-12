b7cdc5045f6a20118f22ebf4e7055a8aa2845f88
naylasofia <50183590+naylasofia@users.noreply.github.com>
2019-07-12 20:33:20 -0300

    Actualizo navegacion y head

--- include/head.php
+++ include/head.php
@@ -7,6 +7,7 @@ session_start();
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
     <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
     <link rel="stylesheet" href="./css/normalize.css">
--- include/navegacion.php
+++ include/navegacion.php
@@ -1,26 +1,34 @@
 <header>
-  <nav class="navbar navbar-template navbar-expand-lg navbar-light bg-light justify-content-center">
+  <nav class="navbar navbar-template navbar-expand-lg navbar-light justify-content-center">
     <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <a href="index.php" class="navbar-brand mx-auto mb-0 h1">
       <span class="brand">Big Fashion</span>
     </a>
-    <div class="d-flex flex-row order-2 order-lg-3">
+    <div class="d-flex order-2 order-lg-3">
           <ul class="navbar-nav flex-row">
             <li class="nav-item"><a href="#" class="nav-link px-2 carrito-nav">
               <i class=" fas fa-shopping-cart"></i></a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 carrito-nav">
               <i class="fas fa-heart"></i></a></li>
+
+              <?php
+                if(!empty($_SESSION) && !empty($_SESSION['usuario'])){
+                  echo '<li class="nav-item1"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li><span class="navbar-text"><b> - Bienvenido</b> ' . $_SESSION['usuario']['usuario'].'</span>';
+                }else{
+                  echo '<li class="nav-item1"><a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt carrito-nav"></i>Login</a></li>';
+                }
+              ?>
           </ul>
       </div>
       <div class="collapse navbar-collapse order-3 order-lg-2" id="collapsibleNavbar">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="index.php"><i class="fas fa-home carrito-nav"></i>Inicio</a>
+            <a class="nav-link" href="index.php"><i class="fas fa-home carrito-nav"></i>INICIO</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-tshirt carrito-nav"></i>Indumentaria</a>
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-tshirt carrito-nav"></i>COLECCION</a>
             <div class="dropdown-menu" aria-labelledby="dropdown01">
               <a class="dropdown-item" href="#">Camperas</a>
               <a class="dropdown-item" href="#">Remeras manga larga</a>
@@ -32,13 +40,14 @@
               <a class="dropdown-item" href="#">Cinturones</a>
             </div>
             </li>
-              <?php
-                if(!empty($_SESSION) && !empty($_SESSION['usuario'])){
-                  echo '<li class="nav-item1"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li><span class="navbar-text"><b> - Bienvenido</b> ' . $_SESSION['usuario']['usuario'].'</span>';
-                }else{
-                  echo '<li class="nav-item1"><a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt carrito-nav"></i>Login/Registro</a></li>';
-                }
-              ?>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php"><i class="fas fa-percentage"></i></i> SALE</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="index.php"><i class="fas fa-envelope"></i></i> CONTACTO</a>
+            </li>
+
         </ul>
       </div>
   </nav>
