c4472ef81d4ee3044158cdbbc11183d558c68fcc
gastonestevez <gaston.estevez7@gmail.com>
2019-07-13 18:32:58 -0300

    Emprolijo el home

--- homepage.php
+++ homepage.php
@@ -1,30 +1,34 @@
 <main class="py-4 tarjetaPrincipal">
-<div class="container-fluid">
-
-
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-90" src="img/miko2019.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-90" src="img/2.jpg" alt="Second slide">
-    </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-    </div>
+          <div class="carousel-inner">
 
+            <div class="carousel-item active">
+              <img class="d-block img-fluid " src="img/miko2019.jpg" alt="First slide">
+            </div>
+
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+            </div>
+
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+
+          </div>
+        </div>
+      </div>
   </div>
-
 </div>
-
+    </main>
       <footer class="main-footer">
         <ul>
           <li><img class="envios" src="img/enviosgratis.png" alt="envios_gratis"></li>
@@ -32,4 +36,3 @@
   				<li><img class="envios" src="img/mercadopago.png" alt="mercadopago"></li>
   			</ul>
   		</footer>
-    </main>
